20f075d5b7772627ccbbfc201ebba75eaa649d69
Chris Hamer-Hodges <chris.hamer-hodges@autodesk.com>
2019-07-09 14:17:45 +0100

    Use diskcache

--- solver.py
+++ solver.py
@@ -6,6 +6,23 @@ from .resultscache import ResultsCache
 import tempfile
 import os
 import shutil
+import cProfile
+import signal
+from diskcache import Deque, Index
+
+class DelayedKeyboardInterrupt(object):
+    def __enter__(self):
+        self.signal_received = False
+        self.old_handler = signal.signal(signal.SIGINT, self.handler)
+
+    def handler(self, sig, frame):
+        self.signal_received = (sig, frame)
+        #logging.debug('SIGINT received. Delaying KeyboardInterrupt.')
+
+    def __exit__(self, type, value, traceback):
+        signal.signal(signal.SIGINT, self.old_handler)
+        if self.signal_received:
+            self.old_handler(*self.signal_received)
 
 class Solver:
 
@@ -13,39 +30,33 @@ class Solver:
         self.megaminx = megaminx.Megaminx()
         temp = tempfile.gettempdir()
         queuefile = os.path.join(temp,'megaminx_queue')
-        self.queue = FileQueue(queuefile,True)
+        self.queue = Deque(directory=queuefile)
         cachedir = os.path.join(temp,'megaminx_cache')
-        self.cache = ResultsCache(cachedir)
+        self.cache = Index(cachedir)
+        #self.queue.clear()
+        #self.cache.clear()
+
 
     def _initialise_forwards(self):
         temp = tempfile.gettempdir()
         queuefile = os.path.join(temp,'megaminx_forward_queue')
         cachedir = os.path.join(temp,'megaminx_forward_cache')
-        if os.path.isfile(queuefile):
-            # Check it to see if it begins with the same state
-            fh = open(queuefile, "rb")
-            ostate = fh.readline()
-            fh.close()
-            if ostate != self.ostate:
-                # Belongs to a different case
-                # Remove it
-                self._clear_forwards()
                 
-        self.forward_queue = FileQueue(queuefile,True)   
-        self.forward_cache = ResultsCache(cachedir)
+        self.forward_queue = Deque(directory=queuefile)   
+        self.forward_cache = Index(cachedir)
+        # Check it to see if it begins with the same state
+        if 'ostate' in self.forward_cache and self.forward_cache['ostate'] != self.ostate:
+            self.forward_queue.clear()
+            self.forward_cache.clear()
+        if not 'ostate' in self.forward_cache:
+            with DelayedKeyboardInterrupt():
+                self.forward_queue.append(self.ostate)
+                self.forward_cache['ostate'] = self.ostate
 
-        if self.forward_queue.empty():
-            self.forward_queue.enqueue(self.ostate)
 
     def _clear_forwards(self):
-        temp = tempfile.gettempdir()
-        queuefile = os.path.join(temp,'megaminx_forward_queue')
-        cachedir = os.path.join(temp,'megaminx_forward_cache')
-        os.unlink(queuefile)
-        if os.path.isfile(queuefile + '_i'):
-            os.unlink(queuefile + '_i')
-        if os.path.isdir(cachedir):
-            shutil.rmtree(cachedir)
+        self.forward_queue.clear()
+        self.forward_cache.clear()
 
     def solve(self,ostate):
         m = self.megaminx
@@ -58,18 +69,18 @@ class Solver:
 
         cache = self.cache
 
-        result = cache.retrieve(m)
+        result = cache.get(ostate)
         if result:
             report(result)
             return
 
         q = self.queue
-        if q.empty():
-            q.enqueue(megaminx.solved)
+        if not len(q):
+            q.append(megaminx.solved)
 
         most_steps = 0
 
-        while not q.empty():
+        while len(q):
             solved = self.process_backward()
             if solved:
                 report(solved)
@@ -82,9 +93,12 @@ class Solver:
         m = self.megaminx
         cache = self.cache
         q = self.queue
-        state = q.dequeue()
+        state = q[0]
+        #print(state)
         m.parse(state)
-        moves = "" if state == megaminx.solved else cache.retrieve(m)
+        #print(len(cache))
+        moves = "" if state == megaminx.solved else cache[state]
+        #print(moves)
         self.backward_steps = len(moves.split(" "))
       
         for col in m.colours:
@@ -96,24 +110,31 @@ class Solver:
                 else:
                     m.faces[col].rotate_anticlockwise()
                 reverse_action = col + ('<', '>')[act]
+                #print('b %s' % reverse_action)
                 new_moves = reverse_action + ' ' + moves
                 new_state = str(m)
                 if new_state == self.ostate:
                     # Solved!
-                    cache.store(m,new_moves)
+                    with DelayedKeyboardInterrupt():
+                        cache[new_state] = new_moves
                     return new_moves
-                elif not cache.retrieve(m):
+                elif not new_state in cache:
                     # New state. Explore more from here
-                    cache.store(m,new_moves)
-                    q.enqueue(new_state)
+                    with DelayedKeyboardInterrupt():
+                        cache[new_state] = new_moves
+                        q.append(new_state)
+                        #print(len(cache))
+        # Done with item in queue now
+        with DelayedKeyboardInterrupt():
+            q.popleft()
 
     def process_forward(self):
         m = self.megaminx
         cache = self.forward_cache
         q = self.forward_queue
-        state = q.dequeue()
+        state = q[0]
         m.parse(state)
-        moves = "" if state == self.ostate else cache.retrieve(m)
+        moves = "" if state == self.ostate else cache[state]
         self.forward_steps = len(moves.split(" "))
       
         for col in m.colours:
@@ -125,20 +146,25 @@ class Solver:
                 else:
                     m.faces[col].rotate_anticlockwise()
                 action = col + ('>', '<')[act]
+                #print('f %s' % action)
                 new_moves = moves + ' ' + action if moves else action
                 new_state = str(m)
-                join_up_moves = self.cache.retrieve(m)
+                join_up_moves = self.cache.get(new_state)
                 if join_up_moves:
                     # Solved!
                     solution = new_moves + ' ' + join_up_moves
                     # Dispose of forwards cache
-                    q.close()
-                    self._clear_forwards()
+                    q.clear()
+                    cache.clear()
                     return solution
-                elif not cache.retrieve(m):
+                elif not new_state in cache:
                     # New state. Explore more from here
-                    cache.store(m,new_moves)
-                    q.enqueue(new_state)
+                    with DelayedKeyboardInterrupt():
+                        cache[new_state] = new_moves
+                        q.append(new_state)
+        # Done with item in queue now
+        with DelayedKeyboardInterrupt():
+            q.popleft()
 
 
     def double_ended_solve(self,ostate):
@@ -152,20 +178,21 @@ class Solver:
 
         cache = self.cache
 
-        result = cache.retrieve(m)
+        result = cache.get(ostate)
         if result:
             report(result)
             return
 
         q = self.queue
-        if q.empty():
-            q.enqueue(megaminx.solved)
+        if not len(q):
+            with DelayedKeyboardInterrupt():
+                q.append(megaminx.solved)
 
         self._initialise_forwards()
 
         most_steps = 0
 
-        while not q.empty():
+        while len(q):
             
             solved = self.process_backward()
             if solved:
@@ -179,6 +206,7 @@ class Solver:
             if  steps > most_steps:
                 print("%d steps" % steps)
                 most_steps = steps              
+            
 
 def report(result):
     print("Solved!")
@@ -195,9 +223,9 @@ if __name__ == "__main__":
     m.faces['Y'].rotate_clockwise()
     m.faces['w'].rotate_clockwise()
     m.faces['r'].rotate_clockwise()
-    m.faces['G'].rotate_clockwise()
-    m.faces['r'].rotate_anticlockwise()
-    m.faces['g'].rotate_anticlockwise()
+    #m.faces['G'].rotate_clockwise()
+    #m.faces['r'].rotate_anticlockwise()
+    #m.faces['g'].rotate_anticlockwise()
     s = Solver()
     s.double_ended_solve(str(m))
     #s.solve(str(m))
